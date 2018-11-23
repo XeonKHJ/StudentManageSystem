d2b21cb23e3225b4a9038a4aa6f95d35a1206fcb
Hongjia <redalertkhj@live.cn>
2018-11-23 18:30:56 +0800

    给注册功能添加上了完整学生信息（未验证）

--- StudentManageSystem/WebContent/Register.jsp
+++ StudentManageSystem/WebContent/Register.jsp
@@ -27,14 +27,13 @@
 			<tr>
 				<td><label for="password">密 码:&nbsp;</label></td>
 				<td><input name="password" tabindex="2" id="password"
-					style="width: 105px;" type="password" maxlength="40" /> <input
-					name="encodedPassword" type="hidden" value="" /></td>
+					style="width: 105px;" type="password" maxlength="40" />
+				</td>
 			</tr>
 			<tr>
 				<td><label for="name">姓名:&nbsp;</label></td>
 				<td><input name="name" tabindex="2" id="name"
-					style="width: 105px;" type="text" maxlength="40" /> <input
-					name="encodedPassword" type="hidden" value="" /></td>
+					style="width: 105px;" type="text" maxlength="40" /></td>
 			</tr>
 			<tr>
 				<td><label for="occupation">类型:&nbsp;</label></td>
@@ -45,9 +44,8 @@
 			</tr>
 			<tr>
 				<td><label for="birthday">生日:&nbsp;</label></td>
-				<td><input name="birthday" tabindex="2" id="password"
-					style="width: 105px;" type="date" maxlength="40" /> <input
-					name="birthday" type="hidden" value="" /></td>
+				<td><input name="birthday" tabindex="2" id="birthday"
+					style="width: 105px;" type="date" maxlength="40" /></td>
 			</tr>
 			<tr>
 				<td colspan="2"><input name="submitBtn" tabindex="6"
--- StudentManageSystem/src/com/StudentManageSystem/Register/Servlet/RegisterCheck.java
+++ StudentManageSystem/src/com/StudentManageSystem/Register/Servlet/RegisterCheck.java
@@ -32,41 +32,21 @@ public class RegisterCheck extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		PrintWriter out = response.getWriter();
 		String occupation = request.getParameter("occupation");
 		String id = request.getParameter("userId");
 		String name = request.getParameter("name");
 		String password = request.getParameter("password");
+		String birthday = request.getParameter("birthday");
 		try {
-			Connection dbConnection;
 			DatabaseConnection dbConnectionInfo = new DatabaseConnection("StudentManagementAdmin", "admin", "XEON-DELL7460", "StudentsManagement");
-			/*String createUserSql = "USE [master]\r\n" + 
-					"GO\r\n" + 
-					"CREATE LOGIN [" + id + "] WITH PASSWORD=N'" + password + "', DEFAULT_DATABASE=[StudentsManagement], CHECK_EXPIRATION=OFF, CHECK_POLICY=OFF\r\n" + 
-					"GO\r\n" + 
-					"use [StudentsManagement]\r\n" + 
-					"\r\n" + 
-					"GO\r\n" + 
-					"use [master]\r\n" + 
-					"\r\n" + 
-					"GO\r\n" + 
-					"USE [StudentsManagement]\r\n" + 
-					"GO\r\n" + 
-					"CREATE USER ["+ id + "] FOR LOGIN [" + id +"]\r\n" + 
-					"GO\r\n" + 
-					"USE [StudentsManagement]\r\n" + 
-					"GO\r\n" + 
-					"ALTER ROLE [" + occupation + "] ADD MEMBER [" + id + "]\r\n" + 
-					"GO\r\n";*/
-			String createPerson = "INSERT INTO " + occupation + "s VALUES ('" + id + "', '" + name + "')";
-			dbConnection = dbConnectionInfo.getCon();
+			String createPerson = "INSERT INTO " + occupation + "s VALUES ('" + id + "', '" + name + "', '" + birthday + "')";
+			Connection dbConnection = dbConnectionInfo.getCon();
 			Statement stat = dbConnection.createStatement();
 			int result = stat.executeUpdate(createPerson);
 		} catch (SQLServerException e) {
 			out.println("<p>网站错误</p>");
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
 			out.println("<p>注册失败</p>");
 			e.printStackTrace();
 		}
