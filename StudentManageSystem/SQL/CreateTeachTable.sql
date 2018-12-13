e8878ee193ee0988733a54782bf707a87b1e96ec
康宏嘉 <redalertkhj@live.cn>
2018-12-13 11:17:43 +0800

    可级联删除的Teach表的SQL。

--- StudentManageSystem/SQL/CreateTeachTable.sql
+++ StudentManageSystem/SQL/CreateTeachTable.sql
@@ -3,7 +3,7 @@ create table Study
 	Sno char(11),
 	Cno nchar(10),
 	Score int,
-	FOREIGN KEY (Sno) REFERENCES Students(Sno),
-	FOREIGN KEY (Cno) REFERENCES Courses(Cno),
+	FOREIGN KEY (Sno) REFERENCES Students(Sno) on delete cascade,
+	FOREIGN KEY (Cno) REFERENCES Courses(Cno) on delete cascade,
 	Primary key(Sno, Cno)
 )
